aa9ce197669b8976080b86a471d8436b6a39cc38
imjchiang <jchiang296@gmail.com>
2020-09-03 10:00:25 -0700

    [passport] set env variable, options, export passport function

--- backend/config/passsport.js
+++ backend/config/passsport.js
@@ -4,3 +4,32 @@ require("dotenv").config();
 //this allows to authenticate endpoints using the token
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const { deserializeUser } = require("passport");
+//const User = mongoose.model("User");
+
+const options = {}
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.secretOrKey = process.env.JWT_SECRET;
+
+module.exports = (passport) =>
+{
+    passport.use(new JwtStrategy(options, (jwt_payload, done) =>
+    {
+        User.findById(jwt_payload.id)
+        .then(user => 
+        {
+            if (user)
+            {
+                //if the user is found, return null (for error) and user
+                return done(null, user);
+            }
+            //if no user is found
+            return done(null, false);
+        })
+        .catch(err =>
+        {
+            console.log(err);
+        })
+    }));
+}
