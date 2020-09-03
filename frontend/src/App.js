bed6f0cb1bfb0f44a3aef49cc4e2c16c474eb715
imjchiang <jchiang296@gmail.com>
2020-09-03 15:39:09 -0700

    [components] add navbar, about, footer components and test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import Welcome from './components/Welcome';
+import About from './components/About';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import './index.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Welcome />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () =>
+{
+    return(
+        <div>
+            <h1>About</h1>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore maiores ab omnis dolorem id
+                inventore esse assumenda modi repellendus vitae soluta tenetur odit alias quod, nulla voluptatem 
+                architecto enim minima?
+            </p>
+        </div>
+    )
+}
+
+export default About;
