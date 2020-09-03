38fa81f5dae51db2ab46685efd42671670b5287c
imjchiang <jchiang296@gmail.com>
2020-09-03 10:31:47 -0700

    [index] set up mongo connection

---
+++ backend/models/index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+//Mongo connection
+mongoose.connect(process.env.MONGO_URI, 
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
+});
+
+//mongoose connection object
+const db = mongoose.connection;
+
+//set up an event listener to fire once when the connections "opens"
+//console log what hose and port it's running on
+
+db.once("open", () =>
+{
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+});
+
+db.on("error", (err) =>
+{
+    console.log(`Database error \n ${err}`);
+});
+
+module.exports.Usser = require("./User");
