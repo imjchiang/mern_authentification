5902b5761c6906a1fedbdef2761222b95de6520a
imjchiang <jchiang296@gmail.com>
2020-09-03 10:21:49 -0700

    [passport] add notes on passport

--- backend/config/passsport.js
+++ backend/config/passsport.js
@@ -5,10 +5,12 @@ require("dotenv").config();
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
-const { deserializeUser } = require("passport");
-//const User = mongoose.model("User");
+const User = mongoose.model("User");
 
+//options iss an object literal containing options to control how the token is extracted from the request or verified
 const options = {}
+//jwtFromRequest (REQUIRED) function that accepts a request as the only parameter and returns either the JWT as a string or null
+//fromAuthHeaderAsBearerToken() creates an extractor that looks for the JWT in the auth header
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 options.secretOrKey = process.env.JWT_SECRET;
 
@@ -16,6 +18,8 @@ module.exports = (passport) =>
 {
     passport.use(new JwtStrategy(options, (jwt_payload, done) =>
     {
+        //jwt_payload is an object literal containing the decoded JWT payload
+        //done is a passport callback that has error first as an argument done(error, user, info)
         User.findById(jwt_payload.id)
         .then(user => 
         {
