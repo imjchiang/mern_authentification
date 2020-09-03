1bd8553af75f2c07a83d473cb1529e4bfbd04421
imjchiang <jchiang296@gmail.com>
2020-09-03 10:39:02 -0700

    [User] add model and export

---
+++ backend/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//User Schema
+const UserSchema = new Schema(
+{
+    name:
+    {
+        type: String,
+        requuired: true
+    },
+    email:
+    {
+        type: String,
+        required: true
+    },
+    password:
+    {
+        type: String,
+        required: true
+    },
+    date:
+    {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = User = mongoose.model("User", UserSchema);
